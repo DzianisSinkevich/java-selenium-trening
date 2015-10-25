85f6776b4a18097aea2fbb4f1db01c78ed770741
DzianisSinkevich <dzianis_sinkevich@dancosoft.com>
2015-10-26 00:24:19 +0200

    First commit

---
+++ src/test/java/denis_trening/LoginTest.java
@@ -0,0 +1,47 @@
+package denis_trening;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.testng.annotations.Test;
+
+import denis_trening_pages.TestBase;
+
+public class LoginTest extends TestBase {
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Test
+	public void testUntitled() throws Exception {
+		driver.get(baseUrl + "/php4dvd/");
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.name("submit")).click();
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+}
