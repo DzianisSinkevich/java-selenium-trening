bbf05ea6d088a2278af64406191e47906d7c6141
DzianisSinkevich <dzianis_sinkevich@dancosoft.com>
2015-10-26 11:48:28 +0300

    Update test

--- src/test/java/denis_trening/LoginTest.java
+++ src/test/java/denis_trening/LoginTest.java
@@ -1,5 +1,7 @@
 package denis_trening;
 
+import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -14,11 +16,23 @@ public class LoginTest extends TestBase {
 	@Test
 	public void testUntitled() throws Exception {
 		driver.get(baseUrl + "/php4dvd/");
-		driver.findElement(By.id("username")).clear();
-		driver.findElement(By.id("username")).sendKeys("admin");
 		driver.findElement(By.name("password")).clear();
 		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("admin");
 		driver.findElement(By.name("submit")).click();
+		driver.findElement(By.linkText("Nederlands")).click();
+		driver.findElement(By.linkText("Gebruikers")).click();
+		driver.findElement(By.name("email")).clear();
+		driver.findElement(By.name("email")).sendKeys("admin");
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("admin");
+		driver.findElement(By.linkText("Log uit")).click();
+		assertTrue(closeAlertAndGetItsText().matches("^Weet je zeker dat je wilt uitloggen[\\s\\S]$"));
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys("admin");
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("admin");
 	}
 
 	private boolean isElementPresent(By by) {
